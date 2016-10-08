3023016af382ecab665f40d5b12b6f0be1185be4
Kelly ONeil <kelly.brianne.oneil@gmail.com>
2016-10-08 14:23:11 -0500

    changed server and client URLs

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,32 +3,62 @@ $(function(){
 var x;
 var y;
 var type;
+var clicks = 1;
 
 append();
 //on click the variables will change according to their id
 $('form').on('click', 'button', function (){
   var $button = $(this);
-  if ($button.attr('class') === 'groupOne'){
-    x = $(this).attr('id');
-  }
   if ($button.attr('class') === 'function'){
     type = $(this).attr('id');
-  }
-  if ($button.attr('class') === 'groupTwo'){
-    y = $(this).attr('id');
-  }
+  } else {
+     if (clicks === 1) {
+         x = $(this).attr('id');
+         clicks++;
+       } else {
+          y = $(this).attr('id');
+          clicks= 1;
+     }
+ }
+
 });
 //on click the data will get sent to the server
 $('#outsideForm').on('click', '#submit', function(event){
   var formData = {x: x, y: y, type: type};
   event.preventDefault();
+  //sends the data to the URL that matches the operation
+if (type === "add"){
   $.ajax({
     type: 'POST',
-    url: '/calculate',
+    url: '/calculate/add',
     data: formData,
     //on success it will get appended to the DOM
     success:  append
-});//end ajax
+});
+} else if (type === "subtract"){
+  $.ajax({
+    type: 'POST',
+    url: '/calculate/subtract',
+    data: formData,
+
+    success:  append
+});
+} else if (type === "divide"){
+  $.ajax({
+    type: 'POST',
+    url: '/calculate/divide',
+    data: formData,
+    success:  append
+});
+} else if (type === "multiply"){
+  $.ajax({
+    type: 'POST',
+    url: '/calculate/multiply',
+    data: formData,
+    success:  append
+});
+}
+
 });//end click
 
 //on clear the box will be emptied
--- routes/calculate.js
+++ routes/calculate.js
@@ -2,25 +2,30 @@ var express = require('express');
 var router = express.Router();
 
 var total = [];
-//takes in the data sent from client.js and does the calcuation 
-router.post('/', function (req, res) {
-  //clears previous total
-  total = [];
-  //depending on the type, it does the appriopriate calculation
-if (req.body.type === 'add'){
+//takes in the data sent from client.js and does the calcuation
+//that matchs its URL 
+router.post('/add', function (req, res) {
+    total = [];
   total.push(Number(req.body.x) + Number(req.body.y));
-}
-if (req.body.type === 'subtract'){
-  total.push(Number(req.body.x) - Number(req.body.y));
-}
-if (req.body.type === 'multiply'){
-  total.push(Number(req.body.x) * Number(req.body.y));
-}
-if (req.body.type === 'divide'){
-  total.push(Number(req.body.x) / Number(req.body.y));
-}
   res.sendStatus(200);
 });
+
+router.post('/subtract', function (req, res) {
+    total = [];
+  total.push(Number(req.body.x) - Number(req.body.y));
+  res.sendStatus(200);
+});
+router.post('/divide', function (req, res) {
+    total = [];
+  total.push(Number(req.body.x) / Number(req.body.y));
+  res.sendStatus(200);
+});
+router.post('/multiply', function (req, res) {
+    total = [];
+  total.push(Number(req.body.x) * Number(req.body.y));
+  res.sendStatus(200);
+});
+
 
 router.get('/', function (req, res) {
 //send the total info back to server.js which will send it to the client side
