6a6c5ee94577dc290677ce68747c9116730682ca
Kelly ONeil <kelly.brianne.oneil@gmail.com>
2016-10-08 17:11:41 -0500

    complete pro mode

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # Weekend Challenge 3
 
-This "Calculator" repo was created to practice using express. The input must be sent into the server and then to a router that does the calculations. Finally, the total is sent back to the client and appended to the DOM.
+This "Calculator" repo was created to practice using express. A user can interact with the keypad to make calculations using addition, subtraction, multiplication and division.  
 
 ## Features
+the user can...
 
-The user can input any number and either add, subtract, multiply or divide. There is also a clear feature that empties out the last answer.
+* click a number, an operator, a number and equals to make a calculation
+* reset the numbers with clear
+* use one decimal place for first and second number input
+
+The calculator also...
+
+* displays the first and second number inputs in the empty box, including decimals
+* contains styling using bootstrap
 
 ## Author
 
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,24 +6,66 @@ var type;
 var clicks = 1;
 
 append();
+
 //on click the variables will change according to their id
 $('form').on('click', 'button', function (){
   var $button = $(this);
+  //sets the type to the right one and clears answer box
   if ($button.attr('class') === 'function'){
     type = $(this).attr('id');
-  } else {
-     if (clicks === 1) {
+    $('#answer').empty();
+  } else if ($button.attr('id') === 'submit'){
+    $('#answer').empty();
+    //sets the clicks to start over
+    clicks=1;
+  } else if ($button.attr('id') === 'clear'){
+    $('#answer').empty();
+    //sets the clicks to start over
+    clicks=1;
+  } else  {
+    //catches for a decimal being added to the y input
+      if ($button.attr('id') === 'decimal' && clicks === 1){
+       y=y+'.'
+       $('#answer').empty();
+       $('#answer').text(y);
+       //moves it to when the y input gets a value after the decimal
+       clicks = 4
+       //catches the x input
+      } else if (clicks === 1) {
          x = $(this).attr('id');
+         $('#answer').empty();
+         $('#answer').text(x);
          clicks++;
-       } else {
+         //catches for a decimal being added to the x input
+      } else if ($button.attr('id') === 'decimal'){
+          x=x+'.'
+          $('#answer').empty();
+          $('#answer').text(x);
+          //moves it to when the x input gets a value after the decimal
+          clicks++;
+          //catches when the x input is getting a value after the decimal
+      } else if(clicks === 3){
+          x+=$(this).attr('id');
+          $('#answer').empty();
+          $('#answer').text(x);
+          clicks=2;
+          //catches the y input
+      } else if (clicks === 2){
           y = $(this).attr('id');
+          $('#answer').empty();
+          $('#answer').text(y);
           clicks= 1;
+          //catches when the y input is getting a value after the decimal
+      } else if(clicks === 4){
+         y+=$(this).attr('id');
+         $('#answer').empty();
+         $('#answer').text(y);
+         clicks=1;
+       }
      }
- }
-
 });
 //on click the data will get sent to the server
-$('#outsideForm').on('click', '#submit', function(event){
+$('#submit').on('click', function(event){
   var formData = {x: x, y: y, type: type};
   event.preventDefault();
   //sends the data to the URL that matches the operation
@@ -61,10 +103,7 @@ if (type === "add"){
 
 });//end click
 
-//on clear the box will be emptied
-$('#outsideForm').on('click', '#clear', function (){
-  $('#answer').empty();
-});
+
 }); //end of doc ready
 //appends the DOM with the calculation result by getting it from the server
 function append() {
@@ -74,7 +113,7 @@ $.ajax({
   success: function (number){
     //clears out the old answer and replaces it with the new one
     $('#answer').empty();
-    $('#answer').text('     ' + number);
+    $('#answer').text(number);
   }
 });
 }
