f6b6008b18fa880bbb059df4742379bf18306ce7
Kelly ONeil <kelly.brianne.oneil@gmail.com>
2016-10-10 11:22:03 -0500

    adjusted number logic to take in more than one digit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,69 +1,99 @@
 $(function(){
 //creates variables for user chosen numbers and operation
-var x;
-var y;
+var x = 0;
+var y = 0;
 var type;
 var clicks = 1;
+var input = 'xValue';
 
 append();
 
 //on click the variables will change according to their id
 $('form').on('click', 'button', function (){
   var $button = $(this);
-  //sets the type to the right one and clears answer box
+
+  //sets the type to the right one and clears answer box and
+  //brings it to the y value
   if ($button.attr('class') === 'function'){
     type = $(this).attr('id');
     $('#answer').empty();
+    clicks = 1;
+    input = 'yValue';
+
+    //on submit the values reset
   } else if ($button.attr('id') === 'submit'){
     $('#answer').empty();
-    //sets the clicks to start over
     clicks=1;
+    input = 'xValue';
+    x = 0;
+    y = 0;
+
+    //on clear the values reset
   } else if ($button.attr('id') === 'clear'){
     $('#answer').empty();
-    //sets the clicks to start over
     clicks=1;
+    input = 'xValue';
+    x = 0;
+    y = 0;
+
+    //this deals with all number buttons and decimals
   } else  {
-    //catches for a decimal being added to the y input
-      if ($button.attr('id') === 'decimal' && clicks === 1){
-       y=y+'.'
-       $('#answer').empty();
-       $('#answer').text(y);
-       //moves it to when the y input gets a value after the decimal
-       clicks = 4
-       //catches the x input
-      } else if (clicks === 1) {
-         x = $(this).attr('id');
-         $('#answer').empty();
-         $('#answer').text(x);
-         clicks++;
-         //catches for a decimal being added to the x input
-      } else if ($button.attr('id') === 'decimal'){
-          x=x+'.'
+
+    //catches for a decimal being added to the x input
+   if ($button.attr('id') === 'decimal' && input === 'xValue'){
+      x=x+'.'
+      $('#answer').empty();
+      $('#answer').text(x);
+      clicks = 2;
+
+      //catches for a decimal being added to the y input
+    } else if ($button.attr('id') === 'decimal' && input === 'yValue'){
+        y=y+'.'
+        $('#answer').empty();
+        $('#answer').text(y);
+        clicks = 2;
+
+        // catches the x value before decimals and clears out the 0 for
+        //first entry and takes in more than one digit
+    } else if (clicks === 1 && input === 'xValue') {
+        if (x === 0){
+          x = $(this).attr('id');
           $('#answer').empty();
           $('#answer').text(x);
-          //moves it to when the x input gets a value after the decimal
-          clicks++;
+        } else {
+         x += $(this).attr('id');
+         $('#answer').empty();
+         $('#answer').text(x);
+        }
+
           //catches when the x input is getting a value after the decimal
-      } else if(clicks === 3){
+      } else if(clicks === 2 && input === 'xValue'){
           x+=$(this).attr('id');
           $('#answer').empty();
           $('#answer').text(x);
-          clicks=2;
-          //catches the y input
-      } else if (clicks === 2){
-          y = $(this).attr('id');
-          $('#answer').empty();
-          $('#answer').text(y);
-          clicks= 1;
-          //catches when the y input is getting a value after the decimal
-      } else if(clicks === 4){
-         y+=$(this).attr('id');
-         $('#answer').empty();
-         $('#answer').text(y);
-         clicks=1;
-       }
+
+          // catches the y value before decimals and clears out the 0 for
+          //first entry and takes in more than one digit
+        } else if (clicks === 1 && input === 'yValue') {
+          if (y === 0){
+            y = $(this).attr('id');
+            $('#answer').empty();
+            $('#answer').text(y);
+          } else {
+            y += $(this).attr('id');
+            $('#answer').empty();
+            $('#answer').text(y);
+          }
+
+             //catches when the y input is getting a value after the decimal
+         } else if(clicks === 2 && input === 'yValue'){
+             y+=$(this).attr('id');
+             $('#answer').empty();
+             $('#answer').text(y);
+           }
      }
 });
+
 //on click the data will get sent to the server
 $('#submit').on('click', function(event){
   var formData = {x: x, y: y, type: type};
@@ -82,7 +112,6 @@ if (type === "add"){
     type: 'POST',
     url: '/calculate/subtract',
     data: formData,
-
     success:  append
 });
 } else if (type === "divide"){
@@ -102,7 +131,6 @@ if (type === "add"){
 }
 
 });//end click
-
 
 }); //end of doc ready
 //appends the DOM with the calculation result by getting it from the server
